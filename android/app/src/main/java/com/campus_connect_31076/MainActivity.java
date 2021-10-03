a4702799b92c32694a9e24b3c981accffdd382d6
Crowdbotics <team@crowdbotics.com>
2021-10-03 21:27:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/campus_connect_31076/MainActivity.java
@@ -0,0 +1,15 @@
+package com.campus_connect_31076;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "campus_connect_31076";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "campus_connect_31076",
+  url: "https://campus_connect_31076.botics.co",
+  api: "https://campus_connect_31076.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
